57169249c1094082e2eaf045bea8f2b6644d7a7e
NEZUKO <trandinhvan0294@gmail.com>
2025-02-24 20:58:32 +0700

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+-   Mở terminal và chạy lệnh sau:
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+-   Copy các file ['filename.xlsx'] vào thự mục cùng cấp với file 'script.py'
+-   Mở `script.py` 
--- script.py
+++ script.py
@@ -4,23 +4,26 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.edge.options import Options
-from threading import Thread
+from selenium.webdriver.edge.service import Service
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
 
-# reading csv file 
-df = pd.read_excel("data.xlsx", names=['name', 'address'])
+
+num_threads = 24
 
 def check_xFanTV(addresses, results, start):
     options = Options()
     options.add_argument('--headless')
     options.headless = True
-    driver = webdriver.Edge(options=options)
-    
+    service = Service(executable_path='msedgedriver.exe')
+    driver = webdriver.Edge(options=options, service=service)
+
     for i, address in enumerate(addresses):
         driver.get(f"https://suivision.xyz/account/{address}?tab=Assets")
         try:
-            # Đợi phần tử chứa chữ "xFanTV" xuất hiện
-            element = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'xFanTV')]"))
+            element = WebDriverWait(driver, 8).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, "//*[contains(text(), 'xFanTV')]"))
             )
             print(f"{i}: {address} - xFanTV: True")
             results[start + i] = True
@@ -33,24 +36,29 @@ def task(df, start, end, results):
     addresses = df.loc[start:end, 'address'].tolist()
     check_xFanTV(addresses, results, start)
 
-# Chia DataFrame thành 6 phần và chạy 6 luồng song song
-threads = []
-num_threads = 8
-chunk_size = len(df) // num_threads
-results = [None] * len(df)
 
-for i in range(num_threads):
-    start = i * chunk_size
-    end = start + chunk_size - 1 if i < num_threads - 1 else len(df) - 1
-    t = Thread(target=task, args=(df, start, end, results))
-    threads.append(t)
-    t.start()
+async def checkfile(fileName):
+    df = pd.read_excel(fileName, names=['name', 'address'])
+    chunk_size = len(df) // num_threads
+    results = [None] * len(df)
 
-for t in threads:
-    t.join()
+    loop = asyncio.get_event_loop()
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        tasks = []
+        for i in range(num_threads):
+            start = i * chunk_size
+            end = start + chunk_size - 1 if i < num_threads - 1 else len(df) - 1
+            tasks.append(loop.run_in_executor(executor, task, df, start, end, results))
+        await asyncio.gather(*tasks)
 
-# Cập nhật DataFrame với kết quả
-df['xFanTV'] = results
+    df['xFanTV'] = results
+    df.to_excel(f"{fileName}_lan2.xlsx", index=False)
 
-# Save the updated DataFrame to a new Excel file
-df.to_excel("updated_data.xlsx", index=False)
+file_list = [
+    'updated_data.xlsx'
+]
+
+for file in file_list:
+    asyncio.run(checkfile(file))
+
+print("All done")
